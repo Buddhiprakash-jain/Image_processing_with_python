ce8487d052a934c4c904d9c97c7a37d421a10bdc
Buddhiprakash jain <71218910+Buddhiprakash-jain@users.noreply.github.com>
2021-06-05 16:09:56 +0530

    Crop_and_Swap

--- Crop_and_Swap.py
+++ Crop_and_Swap.py
@@ -17,13 +17,13 @@ cv2.destroyAllWindows()
 
 #Croping image of hut1
 crop_hut1 = hut1[20:200 , 10:140]
-cv2.imshow("chut1",crop_hut1)
+cv2.imshow("cropped_hut1",crop_hut1)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
 #Croping image of hut2
 crop_hut2 = hut2[20:200 , 10:140]
-cv2.imshow("chut1",crop_hut2)
+cv2.imshow("cropped_hut2",crop_hut2)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
@@ -33,12 +33,12 @@ hut2 = cv2.imread('hut2.jpg')
 
 #swap croping image of hut2 to hut1
 hut1[20:200 , 10:140] = crop_hut2
-cv2.imshow("chut1",hut1)
+cv2.imshow("swap_hut2_to_hut1",hut1)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
 #swap croping image of hut1 to hut2
 hut2[20:200 , 10:140] = crop_hut1
-cv2.imshow("chut1",hut2)
+cv2.imshow("swap_hut1_to_hut2",hut2)
 cv2.waitKey()
 cv2.destroyAllWindows()
